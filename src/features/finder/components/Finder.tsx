2299b4472d39a5ba531ee804f1516dc44a733bbf
Hampus Dufberg Töttrup <Hampus>
2023-03-14 22:30:04 +0100

    wip filter and serch componets

--- src/features/finder/components/Finder.tsx
+++ src/features/finder/components/Finder.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import JobCard from "./JobCard";
+import { Filter } from "../../filter/index";
 
 const Finder = () => {
-  return <div>
+  return (
+    <div>
+      
     Finder
+      <Filter />
+    
     <JobCard/>
-  </div>;
+  </div>
+  );
 };
 
 export default Finder;
