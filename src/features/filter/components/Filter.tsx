38d29f2b1c81d30f87b05e18765f518e31494b96
Hampus Dufberg Töttrup <Hampus>
2023-03-16 16:56:48 +0100

    basic implementation of filters with redux

--- src/features/filter/components/Filter.tsx
+++ src/features/filter/components/Filter.tsx
@@ -1,25 +1,43 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import useToggle from "../../../hooks/useToggle";
-import FilterItem from "./FilterItem";
+import type { RootState } from "../../../store";
+import { useSelector, useDispatch } from "react-redux";
+import FilterCategories from "./FilterCategories";
+
+const handleClearAllFilters = () => {
+  console.log("Clearing all filters");
+};
 
 const Filter = () => {
   const [active, toggleActive] = useToggle();
-
-  const placeholderArray: Array<any> = [];
-
-  type Props = {
-    key: number;
-    item: object;
-  };
+  const filters = useSelector((state: RootState) => state.filter.filters);
+  const dispatch = useDispatch();
 
   return (
     <div className="rounded-xl bg-primary p-4">
       <SearchBar toggleActive={toggleActive} />
-      {active &&
-        placeholderArray.map((item, index) => (
-          <FilterItem key={index} item={item} />
-        ))}
+
+      {active && (
+        <>
+          <div className="my-2 flex justify-between">
+            <div>
+              Filter{" "}
+              <span className="rounded-lg bg-gray-200 p-1">{"amount"}</span>
+            </div>
+            <button className="text-[#5fa3e1]" onClick={handleClearAllFilters}>
+              Clear all
+            </button>
+          </div>
+          {filters.map((category, index: number) => (
+            <FilterCategories
+              key={index}
+              category={category}
+              catIndex={index}
+            />
+          ))}
+        </>
+      )}
     </div>
   );
 };
